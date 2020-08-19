3d6cedf7e400d6360016ffdf180304e7d62e358f
yuriyfomin17 <fominyura2012@gmail.com>
2020-08-19 19:29:33 +0100

    update

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -1,7 +1,7 @@
 export default function cors(app) {
   app.use((req, res, next) => {
-    // eslint-disable-line consistent-return
     res.header('Access-Control-Allow-Origin', '*');
+    req.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
@@ -13,3 +13,4 @@ export default function cors(app) {
     next();
   });
 }
+
